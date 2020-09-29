91b0c260921dbc7ef1ee10be0e1c33b1d4c63f0b
peyau <jonathan.levaux.7@hotmail.com>
2020-09-29 18:28:45 +0200

    update

--- bases/js/main.js
+++ bases/js/main.js
@@ -1,2 +1,22 @@
-console.log('Mon message');
-console.error('Mon erreur');
+// console.log('Mon message');
+// console.error('Mon message erreur');
+//document.addEventListener('click', function() {
+//    alert('MESSAGE');
+//});
+
+const bouton3=document.getElementsByClassName('btn btn-primary');
+
+if(bouton3!=null)
+{
+    bouton3[0].addEventListener("click", function() {
+        var table = document.getElementsByTagName("table");
+        //ternaire
+        table[0].style.display === "none"?table[0].style.display = "block" : table[0].style.display = "none";
+    });
+}
+
+/*const btns = document.getElementsByClassName('btn');
+btns[0].addEventListener('click', function(){
+    alert('MESSAGE');
+});
+*/
